03c781df2e9f6e106d99ba79524fc741194305a2
KamilKvasnicka <kamil.kvasnicka@gmail.com>
2022-11-09 14:17:18 +0100

    Add a Client Property

---
+++ ClientStatusOrganizer.Model/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace ClientStatusOrganizer.Model
+{
+    public class Client
+    {
+        public int ClientID { get; set; }
+        public string ClientName { get; set; }
+        public string ClientSurName { get; set; }
+        public decimal ClientBankStatus { get; set; }
+
+    }
+}
